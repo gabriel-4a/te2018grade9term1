a8fc79a074264404e619548681ac2df71b8ce034
Gabriel <gabrielc@csedge.org>
2018-10-06 15:51:59 -0700

    updated math.js file

--- math.js
+++ math.js
@@ -2,26 +2,45 @@
 let num1 = 5;
 let num2 = 10;
 
+// save the sum of num1 and num2 into a variable
+let sum = num1 + num2
+console.log(num1);
+console.log(num2);
+console.log(sum)
+
+//difference = num1 - num2
+console.log('jkkjhhkhiu');
+
+//save the product of num1 amd num2 into a variable
+let product = num1 * num2;
+console.log(product);
+
+//save  the power of num1 and num2 in a variable
+
 // **** Problem 1 ****
 // Store the sum of num1 and num2 in a variable.
 // Print the sum.
 let sum = num1 + num2;
 console.log(sum);
 
+
 // **** Problem 2 ****
 // Store the difference between sum (above) and 7 in a variable.
 // Print the difference.
-
+let difference = num1 - num2;
+console.log('ggfghftyfytf');
 
 // **** Problem 3 ****
 // Store the product of difference and 3 in a variable.
 // Print the product.
-
+let product = num1 * num2;
+console.log(product);
 
 // **** Problem 4 ****
 // Store the power of the product raised to an exponent of 2 in a variable.
 // Print the power.
-
+let power = num1 ^ num2
+console.log(power)
 
 // **** Problem 5 ****
 // Store the quotient of the power divided by 4 in a variable.
