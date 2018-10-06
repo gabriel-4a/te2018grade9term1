5c54916121de413bf03ca07430dbe1122a72dea5
Gabriel <gabrielc@csedge.org>
2018-10-06 13:18:01 -0700

    updated data.js

--- data.js
+++ data.js
@@ -1,5 +1,14 @@
 // Example
-let message = "Hello World!";
+let message = "Hello Gabriel!";
 console.log(message);
 
 // Type your code below!
+let bDayMonth = 1;
+let bDay = 28;
+let bDayYear = 2004;
+let message2 = "How's it going.";
+let message3 = "Today is the first day of the Team Edge program for you. Is it going well";
+let message4 = "Having fun?";
+let havingFun = true;
+let good = true;
+console.log(gucciFlipFlops);
