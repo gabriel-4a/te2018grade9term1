060afeb674bf4ee21f90fcab263603d3a08e8e2f
Gabriel <gabrielc@csedge.org>
2018-10-13 15:35:15 -0700

    made updates but dihdnt finis

--- change.js
+++ change.js
@@ -1,7 +1,57 @@
-// import readline-sync module, don't touch!
-const READLINE = require("readline-sync");
 
-// fill in object below with all relevant properties (see instructions)
-let changeMachine = {};
 
-// write code below that simulates the change machine in action!
+const READLINE = require('readline-sync')
+
+
+let userInput = READLINE.question("Type anyhting and I'll repeat it back!");
+console.log(userInput);
+
+
+// declare and assign a variable for pennies using readlineSync
+// ask the user how many pennies he/she wants change for
+
+let pennies = READLINE.question('Please enter cents as a positive ineger')
+let remainder = (undefined);
+console.log(pennies)
+
+// declare, assign, and print how many quarters you can make out of the pennies
+
+let quarters = Math.floor(pennies/25)
+
+console.log(quarters, 'QUARTERS')
+
+remainder = remainder%25
+// declare, assign, and print how many dimes you can make out of the pennies
+
+let dimes = Math.floor(pennies/10)
+
+console.log(dimes, 'DIMES')
+
+remainder = remainder%10
+// declare, assign, and print how many nickels you can make out of the pennies
+
+let nickels = Math.floor(pennies/5)
+
+console.log(nickels, 'nickels')
+remainder = remainder%5
+
+// declare, assign, and print how many pennies are left over
+
+
+
+
+
+// make a change drawer using an object to
+// store all of your change
+// each property on the object should represent the particular coin
+// use the dot operator on the object to assign how many of each coin you have
+// many coins are in the drawer
+// print the drawer to see that the values are correct
+
+
+let changeDrawer = {
+  quarters: quarters,
+  dimes: dimes,
+  nickels: nickels,
+  pennies: pennies
+};
---
+++ objects.js
@@ -0,0 +1,7 @@
+let person = {name: "Donavan",
+nickname: "Onezii",
+age: 14,
+numOfSiblings: 5,
+catchPhrase: "lowkey",
+hasNintendoSwitch: false}
+console.log(person)
